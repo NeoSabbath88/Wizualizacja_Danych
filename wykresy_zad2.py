5f538719f15e49ea4efa1e3f5ce6e42933576816
NeoSabbath88 <73527400+NeoSabbath88@users.noreply.github.com>
2021-05-10 12:06:42 +0200

    Update wykresy_zad2.py

--- wykresy_zad2.py
+++ wykresy_zad2.py
@@ -3,14 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 xlsx=pd.ExcelFile('datasets\imiona.xlsx')
-df1 = pd.read_excel(xlsx,)
+  df1 = pd.read_excel(xlsx,)
 
 data = pd.read_excel(xlsx, header=0)
 
-df = pd.DataFrame(data)
+  df = pd.DataFrame(data)
 
 dane = df.groupby(['Plec']).agg({'Liczba': {'sum'}})
 
-wykres = dane.plot.bar()
-wykres.set_ylabel('Mld')
+  wykres = dane.plot.bar()
+  wykres.set_ylabel('Mld')
+
 plt.show()
